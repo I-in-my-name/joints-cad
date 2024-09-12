c2e510d6bd735622b94c3a50cbfe21735e9ccd4c
I-in-my-name <jack.e.d.llewellyn@gmail.com>
2024-09-12 15:43:29 +0100

    changed all point implementatation to use nalgebra Vector4, created the rotatable and translatable traits, made implementation use f64, fixed issues with f64 ordering

--- src/libs/core/mod.rs
+++ src/libs/core/mod.rs
@@ -1,37 +1,62 @@
 extern crate nalgebra as na;
 use std::cmp::Ordering;
+use std::ops::Add;
 
 trait Translatable {
-    fn translate(&self, to_translate_by: na::Vector4<i32>) -> Self;
+    fn translate(&mut self, to_translate_by: na::Vector4<f64>);
 }
 //Rotate here means around the object centre and has to do with orientation and NOT position (not
 //rotating around )
 trait Rotatable {
-    fn rotate(&self, to_rotate_by: na::Matrix3<f64>) -> Self;
+    fn rotate(&mut self, to_rotate_by: na::Matrix3<f64>);
 }
 
 
-#[derive(Clone,Copy,PartialEq,Eq,PartialOrd)]
+#[derive(Clone,Copy,PartialEq,PartialOrd)]
 pub struct Point{
-    x: i32,
-    y: i32,
-    z: i32,
-    d: i32,
+   point: na::Vector4<f64>,
 }
-//move logic to utility for calculating from another point  with default parameters 0 0 0
-impl Ord for Point {
-    fn cmp(&self, other: &Self) -> Ordering{
-        let this_value: f64 = ((i32::pow(self.x,2) + i32::pow(self.y,2) + i32::pow(self.z,2)) as f64).sqrt();
-        let other_value: f64 = ((i32::pow(other.x,2) + i32::pow(other.y,2) + i32::pow(other.z,2)) as f64).sqrt();
-    let value = this_value - other_value;
-        if value < 0.0 {
-            Ordering::Less
-        } else if value > 0.0{
-            Ordering::Equal
-        }else{
-            Ordering::Greater
+impl Point{
+    fn vector_to_point(vector: na::Vector4<f64>) -> Point{
+        Point{
+            point: vector,
         }
     }
+    pub fn new(x: f64, y:f64, z:f64, d:f64) -> Self{
+        Point{
+            point: na::Vector4::new(
+                x,
+                y,
+                z,
+                d,
+            )
+        }
+    }
+    pub fn float_cmp(one: &Self, other: &Self) -> Ordering{
+        let this_value: f64 = (one.point.x.powf(2.0) + one.point.y.powf(2.0) + one.point.z.powf(2.)).sqrt();
+        let other_value: f64 = (other.point.x.powf(2.0) + other.point.y.powf(2.0) + other.point.z.powf(2.0)).sqrt();
+        if this_value < other_value {
+            Ordering::Less
+        } else if this_value > other_value{
+            Ordering::Greater
+        }else{
+            Ordering::Equal
+        }
+    }
+    pub fn sort_point_vector(mut items: Vec<Point>) -> Vec<Point>{
+        items.sort_by(|a, b| Point::float_cmp(a,b));
+        items
+    }
+}
+impl Add for Point{
+    type Output = Self;
+
+    fn add(self, other: Self ) -> Self {
+        Self {
+            point: self.point + other.point,
+        }
+    }
+
 }
 #[derive(Clone)]
 pub struct Surface{
@@ -55,30 +80,43 @@ impl PerspectiveObject{
             centre: Self::calculate_centre(&given_sides.clone()),
             sides: given_sides,
         }
-}
+    }
 
     fn calculate_centre(sides: &Vec<Surface>) -> Point{
         let mut points_vec: Vec<Point> = sides_to_points(sides);
-        points_vec.sort();
-        points_vec.dedup();
+        let mut sorted_vec = Point::sort_point_vector(points_vec);
+        sorted_vec.dedup();
 
         let mut average_x: f64 = 0.0; 
         let mut average_y: f64 = 0.0; 
         let mut average_z: f64 = 0.0;
 
-        for point in points_vec.iter(){
-             average_x += point.x as f64;
-             average_y += point.y as f64;
-             average_z += point.z as f64;
+        for outer_point in sorted_vec.iter(){
+             average_x += outer_point.point.x as f64;
+             average_y += outer_point.point.y as f64;
+             average_z += outer_point.point.z as f64;
         }
-        let number_of_points: f64 = points_vec.len() as f64;
+        let number_of_points: f64 = sorted_vec.len() as f64;
         average_x = average_x / number_of_points;
         average_y = average_y / number_of_points;
         average_z = average_z / number_of_points;
 
-        Point{x:average_x as i32,y:average_y as i32,z:average_z as i32, d:1}
+        Point::new(average_x, average_y, average_z, 1.0)
     }
 }
+
+pub struct Camera {
+    orientation: na::Matrix3<f64>,
+    centre: Point,
+    camera_matrix_superior:  na::Matrix4<f64>,
+}
+impl Translatable for Camera{
+    fn translate(&mut self, to_translate_by: na::Vector4<f64>){
+        self.centre = self.centre + Point::vector_to_point(to_translate_by);
+    }
+}
+
+
 //A quirk of rust being that there is no way to abstract over mutability, these two functions can
 //be considered as having entirely different contexts and are thus coupled differently. You could
 //argue that this is not in violation of DRY principles and actually leads to decoupled and
